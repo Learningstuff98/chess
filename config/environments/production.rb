f4290cacdb7bdddd3e3e764d054f4464f9d06a75
Andy Strube <andystrube64@gmail.com>
2020-12-23 17:16:42 -0800

    Added user authentication with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'flixter-andy-strube.herokuapp.com', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
