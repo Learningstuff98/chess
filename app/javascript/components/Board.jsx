9fec20f9a49323a69db5fb92a48c27ddc4f61ff1
Andy Strube <andystrube64@gmail.com>
2020-12-30 18:35:09 -0800

    Refactored the baord

--- app/javascript/components/Board.jsx
+++ app/javascript/components/Board.jsx
@@ -3,34 +3,28 @@ import Tile from './Tile';
 
 function Board() {
 
-  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  const letterValues = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
   const xValues = [1, 2, 3, 4, 5, 6, 7, 8];
 
   const yValues = [8, 7, 6, 5, 4, 3, 2, 1];
 
-  const handleTile  = (y) => {
+  const row = (y) => {
     let tiles = [];
-    for(let i = 0; i < xValues.length; i++) {
-      tiles.push(
-        <Tile
-          x={xValues[i]}
-          y={y}
-          letter={letters[i]}
-        />
-      );
+    for(let valueIndex = 0; valueIndex < 8; valueIndex++) {
+      tiles.push(<Tile
+        x={xValues[valueIndex]}
+        y={y}
+        letter={letterValues[valueIndex]}
+      />);
     }
     return tiles;
   };
 
-  const row = (y) => {
-    return <div className="board-row">
-      {handleTile(y)}
-    </div>
-  };
-
   const rows = yValues.map(y => {
-    return row(y);
+    return <div className="board-row">
+      {row(y)}
+    </div>
   });
 
   return rows;
