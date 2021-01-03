49311a7f65d5936ad65138d3902b4ba6b50c70c9
Andy Strube <andystrube64@gmail.com>
2021-01-02 20:08:20 -0800

    Refactored piece creation code

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,155 +2,39 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :pieces
 
-  def make_pieces # make a function that spawns a piece with given inputs.
-    self.make_white_pieces
-    self.make_black_pieces
-  end
+  def make_pieces
+    self.make_piece("rook", "white", 1, 1, "♜")
+    self.make_piece("rook", "white", 8, 1, "♜")
+    self.make_piece("knight", "white", 2, 1, "♞")
+    self.make_piece("knight", "white", 7, 1, "♞")
+    self.make_piece("bishop", "white", 3, 1, "♝")
+    self.make_piece("bishop", "white", 6, 1, "♝")
+    self.make_piece("king", "white", 5, 1, "♚")
+    self.make_piece("queen", "white", 4, 1, "♛")
 
-  def make_white_pieces
-    self.pieces.create(
-      piece_type: "rook",
-      color: "white",
-      x: 1,
-      y: 1,
-      icon: "♜"
-    )
-    self.pieces.create(
-      piece_type: "rook",
-      color: "white",
-      x: 8,
-      y: 1,
-      icon: "♜"
-    )
-
-    self.pieces.create(
-      piece_type: "knight",
-      color: "white",
-      x: 2,
-      y: 1,
-      icon: "♞"
-    )
-    self.pieces.create(
-      piece_type: "knight",
-      color: "white",
-      x: 7,
-      y: 1,
-      icon: "♞"
-    )
-
-    self.pieces.create(
-      piece_type: "bishop",
-      color: "white",
-      x: 3,
-      y: 1,
-      icon: "♝"
-    )
-    self.pieces.create(
-      piece_type: "bishop",
-      color: "white",
-      x: 6,
-      y: 1,
-      icon: "♝"
-    )
-
-    self.pieces.create(
-      piece_type: "king",
-      color: "white",
-      x: 5,
-      y: 1,
-      icon: "♚"
-    )
-
-    self.pieces.create(
-      piece_type: "queen",
-      color: "white",
-      x: 4,
-      y: 1,
-      icon: "♛"
-    )
+    self.make_piece("rook", "black", 1, 8, "♜")
+    self.make_piece("rook", "black", 8, 8, "♜")
+    self.make_piece("knight", "black", 2, 8, "♞")
+    self.make_piece("knight", "black", 7, 8, "♞")
+    self.make_piece("bishop", "black", 3, 8, "♝")
+    self.make_piece("bishop", "black", 6, 8, "♝")
+    self.make_piece("king", "black", 5, 8, "♚")
+    self.make_piece("queen", "black", 4, 8, "♛")
 
     [1, 2, 3, 4, 5, 6, 7, 8].each do |x_value|
-      self.pieces.create(
-        piece_type: "pawn",
-        color: "white",
-        x: x_value,
-        y: 2,
-        icon: "♙"
-      )
+      self.make_piece("pawn", "black", x_value, 7, "♙")
+      self.make_piece("pawn", "white", x_value, 2, "♙")
     end
   end
 
-  def make_black_pieces
+  def make_piece(piece_type, color, x, y, icon)
     self.pieces.create(
-      piece_type: "rook",
-      color: "black",
-      x: 1,
-      y: 8,
-      icon: "♜"
+      piece_type: piece_type,
+      color: color,
+      x: x,
+      y: y,
+      icon: icon
     )
-    self.pieces.create(
-      piece_type: "rook",
-      color: "black",
-      x: 8,
-      y: 8,
-      icon: "♜"
-    )
-
-    self.pieces.create(
-      piece_type: "knight",
-      color: "black",
-      x: 2,
-      y: 8,
-      icon: "♞"
-    )
-    self.pieces.create(
-      piece_type: "knight",
-      color: "black",
-      x: 7,
-      y: 8,
-      icon: "♞"
-    )
-
-    self.pieces.create(
-      piece_type: "bishop",
-      color: "black",
-      x: 3,
-      y: 8,
-      icon: "♝"
-    )
-    self.pieces.create(
-      piece_type: "bishop",
-      color: "black",
-      x: 6,
-      y: 8,
-      icon: "♝"
-    )
-
-    self.pieces.create(
-      piece_type: "king",
-      color: "black",
-      x: 5,
-      y: 8,
-      icon: "♚"
-    )
-
-    self.pieces.create(
-      piece_type: "queen",
-      color: "black",
-      x: 4,
-      y: 8,
-      icon: "♛"
-    )
-
-    [1, 2, 3, 4, 5, 6, 7, 8].each do |x_value|
-      self.pieces.create(
-        piece_type: "pawn",
-        color: "black",
-        x: x_value,
-        y: 7,
-        icon: "♙"
-      )
-    end
   end
 
 end
