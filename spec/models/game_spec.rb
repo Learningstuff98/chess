9df231a6c79d2995a2f9c715c15fbb245e3475c3
Andy Strube <andystrube64@gmail.com>
2021-01-04 14:18:26 -0800

    Added tests for piece making code

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+  describe "make_piece function" do
+    it "should create a chess piece" do
+      game = FactoryBot.create(:game)
+      game.make_piece("queen", "black", 4, 8, "♛")
+      expect(Piece.all.count).to eq 1
+    end
+  end
 
+  describe "make_pieces function" do
+    it "should make 32 chess pieces" do
+      game = FactoryBot.create(:game)
+      game.make_pieces
+      expect(Piece.all.count).to eq 32
+    end
+  end
 end
