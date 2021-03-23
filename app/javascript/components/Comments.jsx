77f597d5fa18e653ae341041efc620bcd3eb9799
Andy Strube <andystrube64@gmail.com>
2021-03-23 16:53:01 -0700

    comment box is scrollable

--- app/javascript/components/Comments.jsx
+++ app/javascript/components/Comments.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Comments(props) {
-  return <div className="chat-box chat-box-dimensions container">
+  return <div className="chat-box chat-box-dimensions">
     {props.comments.map((comment) => {
-      return <div key={comment.id}>
+      return <div className="green" key={comment.id}>
         {comment.content}
         <br/><br/>
       </div>
