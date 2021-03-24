f16fb2360be0c9ee3e223372e0b9b8ede496ad3d
Andy Strube <andystrube64@gmail.com>
2021-03-23 17:51:26 -0700

    Added username to each comment

--- app/javascript/components/Comments.jsx
+++ app/javascript/components/Comments.jsx
@@ -4,6 +4,9 @@ function Comments(props) {
   return <div className="chat-box chat-box-dimensions">
     {props.comments.map((comment) => {
       return <div className="green" key={comment.id}>
+        <div>
+          {`By ${comment.username}`}
+        </div>
         {comment.content}
         <br/><br/>
       </div>
