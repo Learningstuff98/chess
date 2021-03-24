479c6d3aa0c5b1e78bd1e978723ca91de2b1b758
Andy Strube <andystrube64@gmail.com>
2021-03-24 16:50:58 -0700

    Fixed comment order bug

--- app/javascript/components/Comments.jsx
+++ app/javascript/components/Comments.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
 function Comments(props) {
+
+  const sortByID = (comments) => {
+    return comments.sort((x, y) => {
+      return x.id - y.id;
+    })
+  };
+
+  const cloneComments = () => {
+    let newComments = [];
+    for(const comment of props.comments) {
+      newComments.push(comment);
+    }
+    return newComments;
+  };
+
   return <div className="chat-box chat-box-dimensions">
-    {props.comments.map((comment) => {
+    {sortByID(cloneComments()).map((comment) => {
       return <div className="green" key={comment.id}>
-        <div>
-          {`By ${comment.username}`}
-        </div>
+        <div>{comment.username}</div>
         {comment.content}
         <br/><br/>
       </div>
