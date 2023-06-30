5e905e482cc10f1489a2589520c4d5a4b1fe69b4
Andy Strube <andystrube64@gmail.com>
2023-06-29 19:46:01 -0700

    Added request spec for the pages controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/requests/pages_request_spec.rb
+++ spec/requests/pages_request_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Pages", type: :request do
+  describe "pages#home" do
+    it "should load the page" do
+      get root_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe "pages#lobby" do
+    it "should authenticate the user" do
+      get lobby_path
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "should load the page" do
+      user = FactoryBot.create(:user)
+      sign_in user
+      get lobby_path
+      expect(response).to be_successful
+    end
+  end
 end
