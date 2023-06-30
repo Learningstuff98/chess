006df82e5fd52c5533a6f029f5144871616f0bb1
Andy Strube <andystrube64@gmail.com>
2023-06-29 20:28:38 -0700

    Added request spec for comments

--- spec/factories.rb
+++ spec/factories.rb
@@ -19,10 +19,6 @@ FactoryBot.define do
     association :user
   end
 
-  factory :lobby_token do
-    association :game
-  end
-
   factory :piece do
     piece_type { "queen" }
     color { "black" }
--- spec/requests/comments_request_spec.rb
+++ spec/requests/comments_request_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
+  describe "comments#create" do
+    it "should authenticate the user", :aggregate_failures do
+      game = FactoryBot.create(:game)
+      post game_comments_path(
+        {
+          game_id: game.id,
+          comment: {
+            content: "Hello World"
+          }
+        }
+      )
+      expect(response).to redirect_to new_user_session_path
+      expect(Comment.count).to eq 0
+    end
+
+    it "should let users make comments", :aggregate_failures do
+      user = FactoryBot.create(:user)
+      sign_in user
+      game = FactoryBot.create(:game)
+      post game_comments_path(
+        {
+          game_id: game.id,
+          comment: {
+            content: "Hello World"
+          }
+        }
+      )
+      expect(response).to be_successful
+      expect(Comment.count).to eq 1
+      expect(Comment.first.content).to eq "Hello World"
+      expect(Comment.first.username).to eq user.username
+    end
+  end
 end
