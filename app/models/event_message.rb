bd7cf7ade1bdd1bac3f1fd7abb1bf59eecf328ea
Andy Strube <andystrube64@gmail.com>
2023-09-24 20:33:51 -0700

    Added information to event messages

--- app/models/event_message.rb
+++ app/models/event_message.rb
@@ -3,8 +3,23 @@ class EventMessage < ApplicationRecord
 
   def self.create_event_message(game, selected_piece, piece)
     event_message = game.event_messages.create(
-      content: "The #{selected_piece.piece_type} isn't allowed to jump over the #{piece.piece_type}."
+      content: "The #{selected_piece.piece_type} isn't allowed to jump over the #{piece.piece_type} at #{translate_coordinates_to_board_format(piece)}."
     )
     SendEventMessageJob.perform_later(game, event_message)
+  end
+
+  NUMBER_TO_LETTER_VALUES = {
+    1 => "a",
+    2 => "b",
+    3 => "c",
+    4 => "d",
+    5 => "e",
+    6 => "f",
+    7 => "g",
+    8 => "h"
+  }.freeze
+
+  def self.translate_coordinates_to_board_format(piece)
+    "#{NUMBER_TO_LETTER_VALUES[piece.x]}#{piece.y}"
   end
 end
