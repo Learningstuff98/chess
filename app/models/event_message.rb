1604d526ba565e77f8c6ff1d75260efb35a129df
Andy Strube <andystrube64@gmail.com>
2023-10-01 16:30:10 -0700

    Added tests for the event_message model

--- app/models/event_message.rb
+++ app/models/event_message.rb
@@ -3,23 +3,19 @@ class EventMessage < ApplicationRecord
 
   def self.create_capture_message(game, piece)
     game.event_messages.create(
-      content: "The #{piece.piece_type} at #{translate_coordinates_to_board_format(piece)} was captured."
+      content: "The #{piece.color} #{piece.piece_type} at #{translate_coordinates_to_board_format(piece)} was captured."
     )
   end
 
   def self.create_movement_message(game, piece)
     game.event_messages.create(
-      content: "The #{piece.piece_type} moved to #{translate_coordinates_to_board_format(piece)}."
+      content: "The #{piece.color} #{piece.piece_type} moved to #{translate_coordinates_to_board_format(piece)}."
     )
   end
 
   def self.create_illegal_jump_over_message(game, selected_piece, piece)
     game.event_messages.create(
-      content: "
-        The #{selected_piece.piece_type} at #{translate_coordinates_to_board_format(selected_piece)}
-        isn't allowed to jump over the #{piece.piece_type} at
-        #{translate_coordinates_to_board_format(piece)}.
-      "
+      content: "The #{selected_piece.color} #{selected_piece.piece_type} at #{translate_coordinates_to_board_format(selected_piece)} isn't allowed to jump over the #{piece.color} #{piece.piece_type} at #{translate_coordinates_to_board_format(piece)}."
     )
   end
 
